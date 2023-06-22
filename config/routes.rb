6d9fa76c2e66ba06f079b8276793c5af2de33027
JunyaShibata <135484706+junyashibata@users.noreply.github.com>
2023-06-22 08:46:53 +0000

    start maiking bookers2-2

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+
+  get 'users/index'
+  get 'users/edit'
+  devise_for :users
+  get 'homes/top'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "homes/about" => 'homes#about', as:"about"
+  
+  resources :books
+  resources :users,only: [:index, :edit, :show, :create, :updete]
+end
